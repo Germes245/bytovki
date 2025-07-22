b716f8b161a367a02a0e6e0eb9924a1741f9ba93
artem <dan119@bk.ru>
2025-07-22 10:57:51 +0300

    init

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { config } from "dotenv";
+import { mainPage } from './source/controllers/main.js';
+
+config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./source/templates");
+app.use(express.static("public"));
+
+app.get('/',mainPage)
+app.listen(process.env.PORT, () => console.log('start http://localhost:'+process.env.PORT));
---
+++ source/controllers/main.js
@@ -0,0 +1,8 @@
+import { query } from "../connect_to_database.js";
+
+export async function mainPage(req, res) {
+  //let request = await query`select 1`;
+  res.render("main", {
+    title: "Главная",
+  });
+}
